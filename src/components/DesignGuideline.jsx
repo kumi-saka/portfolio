67fcb034879ad74a088914fd40d2691302e3e0dd
kumi <kawahara.93@gmail.com>
2026-03-02 11:46:08 +0900

    修正

---
+++ src/components/DesignGuideline.jsx
@@ -0,0 +1,15 @@
+import './DesignGuideline.css'
+
+function DesignGuideline() {
+  return (
+    <section className="design-guideline">
+      <h2 className="section-title">Design Guideline</h2>
+      <div className="guideline-philosophy">
+        <h3>Design Philosophy</h3>
+        <p>わかりやすく、シンプルに。必要な情報が自然に伝わる構成を大切にしています。</p>
+      </div>
+    </section>
+  )
+}
+
+export default DesignGuideline
